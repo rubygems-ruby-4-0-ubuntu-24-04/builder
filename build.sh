5c8fac3a2870422938a7b69988a208e78e560a56
Sutou Kouhei <kou@clear-code.com>
2026-02-21 16:52:07 +0900

    Install system dependencies automatically

--- build.sh
+++ build.sh
@@ -6,5 +6,13 @@ name=$1
 version=$2
 
 gem fetch ${name}:${version}
+system_dependencies=()
+case "${name}" in
+  psych) system_dependencies+=(libyaml-dev);;
+esac
+if [ ${#system_dependencies[@]} -gt 0 ]; then
+  sudo apt update
+  sudo apt install -y -V "${system_dependencies[@]}"
+fi
 gem build_binary ${name}-${version}.gem
 cp ${name}-${version}-*.gem /host/
